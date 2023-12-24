9eb293763b9b1389f88655187100dec3af6fcf17
JorgeAlfaro <94091150+JorgeAlfaroAguirre@users.noreply.github.com>
2023-12-24 01:22:55 -0300

    Delete 02-fundamentals/dist directory

--- 02-fundamentals/dist/plugins/get-age.plugin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getAge = void 0;
-const getAge = (birthdate) => {
-    console.log(new Date().getFullYear());
-    return new Date().getFullYear() - new Date(birthdate).getFullYear();
-};
-exports.getAge = getAge;
